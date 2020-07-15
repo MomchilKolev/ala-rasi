dd7376bf6f2a0be644e22ff57d6d07a08caedfa2
Momchil Kolev <momchil.kolev@protonmail.com>
2020-07-15 19:19:52 +0300

    Initial FE commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./App.module.scss";
+
+function App() {
+    return (
+        <div className={styles.App}>
+            <header className="App-header">Ala Rasi</header>
+        </div>
+    );
+}
+
+export default App;
