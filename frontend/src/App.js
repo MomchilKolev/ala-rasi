b419f470e0ab043a70771deac2896546985ead86
Momchil Kolev <momchil.kolev@protonmail.com>
2020-07-15 20:12:13 +0300

    ADD / /charts /movie/:id routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
+import { Switch, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 
 import styles from "./App.module.scss";
 
+const Home = lazy(() => import("./views/Home/Home"));
+const Movie = lazy(() => import("./views/Movie/Movie"));
+const Charts = lazy(() => import("./views/Charts/Charts"));
+
 function App() {
     return (
         <div className={styles.App}>
             <Navbar />
+            <Suspense fallback="Loading...">
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/movie/:id" component={Movie} />
+                    <Route path="/charts" component={Charts} />
+                </Switch>
+            </Suspense>
         </div>
     );
 }
---
+++ frontend/src/views/Charts/Charts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Charts = props => {
+    return <div>Charts</div>;
+};
+
+export default Charts;
